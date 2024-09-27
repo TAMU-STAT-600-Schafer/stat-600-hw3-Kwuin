265bd8c7f46be84b1e4b9174ec4fdf71ef308e94
Jingkun Lin <jkunlin@tamu.edu>
2024-09-27 16:08:49 -0500

    style modified

--- Letter_example.R
+++ Letter_example.R
@@ -13,31 +13,33 @@ Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
 
 # [ToDo] Make sure to add column for an intercept to X and Xt
-X = cbind(1, X)
-Xt = cbind(1, Xt)
+X <- cbind(1, X)
+Xt <- cbind(1, Xt)
 
 library(profvis)
 # Source the LR function
 source("FunctionsLR.R")
 
 # calling classification function and prediction
-#profvis({
+# profvis({
 out <- LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
-#})
+# })
 # [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
 
 # The code below will draw pictures of objective function, as well as train/test error over the iterations
-plot(out$objective, type = 'o')
-plot(out$error_train, type = 'o')
-plot(out$error_test, type = 'o')
+plot(out$objective, type = "o")
+plot(out$error_train, type = "o")
+plot(out$error_test, type = "o")
 
 # Feel free to modify the code above for different lambda/eta/numIter values to see how it affects the convergence as well as train/test errors
 library(microbenchmark)
 # [ToDo] Use microbenchmark to time your code with lambda=1 and 50 iterations. To save time, only apply microbenchmark 5 times.
-microbenchmark({ 
-  LRMultiClass(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)},
+microbenchmark(
+  {
+    LRMultiClass(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
+  },
   times = 5
-  )
+)
 
 # [ToDo] Report the median time of your code from microbenchmark above in the comments below
 
--- Tests.R
+++ Tests.R
@@ -1,26 +1,26 @@
 # This is a script to save your own tests for the function
 source("FunctionsLR.R")
 
-N <- 1000  
-P <- 30    
+N <- 1000
+P <- 30
 K <- 26
 
-beta <- matrix(rnorm((P+1) * K), P+1, K)  
+beta <- matrix(rnorm((P + 1) * K), P + 1, K)
 
 
 data_generation <- function(N, P, K, beta) {
-  X <- matrix(rnorm(N * P,sd = 10), N, P)
-  intercept <- matrix(1, N, 1)  
-  X <- cbind(intercept, X) 
+  X <- matrix(rnorm(N * P, sd = 10), N, P)
+  intercept <- matrix(1, N, 1)
+  X <- cbind(intercept, X)
   print(dim(X))
   print(dim(beta))
-  
+
   probabilities <- softmax_matrix(X, beta)
-  
+
   Y <- apply(probabilities, 1, function(prob) {
     which.max(rmultinom(1, size = 1, prob = prob))
   })
-  
+
   list(X = X, probabilities = probabilities, Y = Y)
 }
 
@@ -28,6 +28,6 @@ data_generation <- function(N, P, K, beta) {
 data <- data_generation(N, P, K, beta)
 
 
-res = LRMultiClass(data$X, data$Y, data$X, data$Y, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
+res <- LRMultiClass(data$X, data$Y, data$X, data$Y, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
 
 # Error rate reduced to 3.4% which means the algorithm runs well with the synthetic data

7912bbb4033a582e47fe2de0953e499c319115e4
Jingkun Lin <jkunlin@tamu.edu>
2024-09-26 23:23:32 -0500

    report bench mark time

--- Letter_example.R
+++ Letter_example.R
@@ -20,9 +20,9 @@ library(profvis)
 # Source the LR function
 source("FunctionsLR.R")
 
-profvis({
+#profvis({
 out <- LRMultiClass(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
-})
+#})
 # [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out <- LRMultiClass(...)
 
 # The code below will draw pictures of objective function, as well as train/test error over the iterations
@@ -31,9 +31,13 @@ plot(out$error_train, type = 'o')
 plot(out$error_test, type = 'o')
 
 # Feel free to modify the code above for different lambda/eta/numIter values to see how it affects the convergence as well as train/test errors
-
+library(microbenchmark)
 # [ToDo] Use microbenchmark to time your code with lambda=1 and 50 iterations. To save time, only apply microbenchmark 5 times.
+microbenchmark({ 
+  LRMultiClass(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)},
+  times = 5
+  )
 
 # [ToDo] Report the median time of your code from microbenchmark above in the comments below
 
-# Median time:  (in sec)
+# Median time: 1.54 (in sec)

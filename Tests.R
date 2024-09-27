49ded6edbd577c7d409177a771c9c8e6de1feeed
Jingkun Lin <jkunlin@tamu.edu>
2024-09-27 16:06:06 -0500

    Test functions and results

--- Tests.R
+++ Tests.R
@@ -1,15 +1,21 @@
 # This is a script to save your own tests for the function
 source("FunctionsLR.R")
 
+N <- 1000  
+P <- 30    
+K <- 26
+
+beta <- matrix(rnorm((P+1) * K), P+1, K)  
+
+
 data_generation <- function(N, P, K, beta) {
-  X <- matrix(rnorm(N * P), N, P)
+  X <- matrix(rnorm(N * P,sd = 10), N, P)
   intercept <- matrix(1, N, 1)  
   X <- cbind(intercept, X) 
-  print(X)
-  print(beta)
-  predictors <- X %*% beta
+  print(dim(X))
+  print(dim(beta))
   
-  probabilities <- softmax_matrix(predictors, beta)
+  probabilities <- softmax_matrix(X, beta)
   
   Y <- apply(probabilities, 1, function(prob) {
     which.max(rmultinom(1, size = 1, prob = prob))
@@ -18,12 +24,10 @@ data_generation <- function(N, P, K, beta) {
   list(X = X, probabilities = probabilities, Y = Y)
 }
 
-N <- 1000  
-P <- 3    
-K <- 26
-
-beta <- matrix(rnorm((P+1) * K), P+1, K)  
 
 data <- data_generation(N, P, K, beta)
 
+
 res = LRMultiClass(data$X, data$Y, data$X, data$Y, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL)
+
+# Error rate reduced to 3.4% which means the algorithm runs well with the synthetic data
